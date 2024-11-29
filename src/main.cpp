82f19fafd961a3da5d8dfe00e1a8fb8f42e8b605
shayba025 <shayba25899@gmail.com>
2024-11-29 20:46:32 +0000

    main update

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 // Simulation* backup = nullptr;
 
 int main(int argc, char** argv){
-    if(argc!=2){
-        cout << "usage: simulation <config_path>" << endl;
-        return 0;
-    }
+   // if(argc!=2){
+   //     cout << "usage: simulation <config_path>" << endl;
+  //      return 0;
+  //  }
     // string configurationFile = argv[1];
     // Simulation simulation(configurationFile);
     // simulation.start();
@@ -17,5 +17,22 @@ int main(int argc, char** argv){
     // 	delete backup;
     // 	backup = nullptr;
     // }
-    return 0;
+    //return 0;
+
+
+
+
+// check stuff:
+ Settlement tlv("Tel Aviv", CITY);
+
+ // Access the object's properties using methods
+    cout << "Name: " << tlv.getName() << endl;
+    cout << "Type: " << (tlv.getType() == TOWN ? "Town" : "Other") << endl;
+
+    // Call toString
+    cout << tlv.toString() << endl;
 }
+
+
+
+
