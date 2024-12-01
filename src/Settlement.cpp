1aa7c089b26895ed45a02b891f76f58ed7a64470
amitbarel8 <amitbarel8610@gmail.com>
2024-12-01 21:37:18 +0200

    Settlement.cpp

--- src/Settlement.cpp
+++ src/Settlement.cpp
@@ -1,42 +1,72 @@
 #include "Settlement.h"
 #include <string>
+#include <sstream>
 
+// sellement constructur
+// variables: name, type
 Settlement::Settlement(const string &name, SettlementType type)
     : name(name), type(type)
 {}
 
 
-
+// getter for name
 const string &Settlement::getName() const {
     return this->name;
 }
 
+// getter for settlement type
 SettlementType Settlement::getType() const{
     return this->type;
 }
 
 
-
- //verify toString       
-const string Settlement::toString() const{
-
-//enum class SettlementType {VILLAGE, CITY, METROPOLIS,} ->> reference to the header
-    string myType="";
- //the switch was incorrecte
+const std::string typeToString(const SettlementType &type){
+ //convert the status enum to viable string for the toString method
+ // determaning the string using the case method of enum
+string typeStr="";
+ 
     switch(type){
         case SettlementType::VILLAGE: 
-            myType = "VILLAGE";
+            typeStr = "VILLAGE";
             break;
         case SettlementType::CITY:
-            myType = "CITY";
+            typeStr = "CITY";
             break;
         case SettlementType::METROPOLIS: 
-            myType = "METROPOLIS";
+            typeStr = "METROPOLIS";
             break;
     }
 
-    return  "name: " + name + "type" + myType;  
+    return typeStr;  
+
 }
 
 
 
+
+const string Settlement::toString() const{
+    std::ostringstream oss; //will be destroyed automatically when we leave the function since it's local
+    // using the oss method for an efficient string "combiner"
+    oss << "name: " << this->name 
+        <<  " type: " << typeToString(this->type);
+    return  oss.str();
+}
+
+//getting the construction limit based on type
+const int Settlement::getConstructionLimit() const{
+    switch (this->type)
+    {
+    case SettlementType::VILLAGE:
+        return 1;
+        break;
+    case SettlementType::CITY:
+        return 2;
+        break;
+    case SettlementType::METROPOLIS:
+        return 3;
+        break;
+    default:
+        return 0;
+        break;
+    }
+}
